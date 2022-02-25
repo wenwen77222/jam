f85c21e94c83f677168d757d4b4b92791516e51e
wenwen22777 <yaqi99999@gmail.com>
2022-02-25 11:42:47 +0800

    add all & rwd+index

---
+++ hamburger.js
@@ -0,0 +1,21 @@
+// document.addEventListener("DOMContentLoaded", function(){
+  
+//     var btn_hamburger = document.getElementsByClassName("hamburger")[0];
+//     btn_hamburger.addEventListener("click", function(){
+//       this.classList.toggle("is-active");
+//     });
+    
+//   });
+
+ 
+  // $("button.hamburger").on("click", function(){
+  //   $(this).toggleClass("is-active");
+  // });
+
+  // $(function(){
+  
+  //   $("button.hamburger").on("click", function(){
+  //     $(this).toggleClass("is-active");
+  //   });
+  
+  // });
